83051039cacbc6fed96614292d4dfe577d7da447
Konrad Bender <36686910+konradbender@users.noreply.github.com>
2022-12-02 12:24:45 +0000

    finish model and advance write up

--- modelling.R
+++ modelling.R
@@ -56,27 +56,37 @@ var(deviance.resids)
 
 # Interpretation
 estimates <- summary(m2)$coefficients[, c(1,2)]
-make_ci <- function(row) {
-  paste("[", round(row[["Estimate"]] - 1.96*row[["Std. Error"]], 3), ",",
-        round(row[["Estimate"]] + 1.96*row[["Std. Error"]], 3),
-  "]"
-  )
-}
-c_interval <- apply(estimates, FUN = make_ci, MARGIN = 1)
-c_interval <- data.frame(c_interval)
-c_interval[, "index"] <- rownames(c_interval)
-
 estimates <- data.frame(estimates)
 estimates[, "index"] <- rownames(estimates)
-estimates[, "odds ratio"] <- exp(estimates[, "Estimate"])
+estimates[, "M"] <- exp(estimates[, "Estimate"])
 estimates[, "beta_i"] <- 0:(dim(estimates)[1]-1)
 estimates[, "beta_i"] <- sapply(estimates[, "beta_i"], FUN = function (row){
   paste0("i=", row)
 })
 
+c_interval <- data.frame(estimates[, "index"])
+c_interval[, "CI.Estimate"] <- NA
+c_interval[, "CI.M"] <- NA
+
+for (i in 1:dim(c_interval)[1]){
+  est <- estimates[i, "Estimate"]
+  std <- estimates[i, "Std..Error"]
+  c_interval[i, "CI.Estimate"] <- paste0("[", round(est - 1.96*std, 3), ", ",
+                                      round(est + 1.96*std, 3),
+                                      "]")
+  c_interval[i, "CI.M"] <- paste0("[", round(exp(est - 1.96*std), 3), ", ",
+                                           round(exp(est + 1.96*std), 3),
+                                           "]")
+}
+c_interval <- data.frame(c_interval)
+colnames(c_interval)[1] <- "index"
+
 joined <- inner_join(estimates, c_interval, by="index")
 
-joined <- joined[, c(3, 5, 1, 2, 4, 6)]
+joined <- joined[, c("index", "beta_i", "Std..Error", "Estimate", "CI.Estimate", "M", "CI.M")]
+
+joined[joined$index == "age", "index"] <- "Age (for men)"
+joined[joined$index == "age:genderfemale", "index"] <- "Age-Gender"
 
 out <- kable(joined, "latex",
       caption="Parameter estimates and confidence intervals",
@@ -85,3 +95,30 @@ out <- kable(joined, "latex",
 fileConn <- file("report/estimates.txt")
 writeLines(out, fileConn)
 close(fileConn)
+
+varhat <- vcov(m2)
+
+beta.a <- m2$coefficients[["age"]] + m2$coefficients[["age:genderfemale"]]
+print(paste("Estimate for the female age coefficient:", round(beta.a, 4)))
+var.beta.a <- varhat["age", "age"] + varhat["age:genderfemale", "age:genderfemale"] +
+  2*varhat["age", "age:genderfemale"]
+std.error.beta.a <- sqrt(var.beta.a)
+
+print(paste0("Confidence interval for the age of a female: [",
+            round(beta.a - std.error.beta.a, 4), ", ",
+            round(beta.a + std.error.beta.a, 4),
+            "]"))
+odds.beta.a <- exp(beta.a)
+print(paste("means ratio for the age of a female:", round(odds.beta.a, 4)))
+print(paste0("Confidence interval for the means ratio: [",
+             round(exp(beta.a - std.error.beta.a), 4), ", ",
+             round(exp(beta.a + std.error.beta.a), 4),
+             "]"))
+
+pearson.phi_hat <- 1/(n-p.final) * sum(residuals.glm(m2, type = "pearson")^2)
+manual.phi_hat <- 1/(n-p.final) * sum(residuals.glm(m2, type = "response")^2/
+                                        predict.glm(m2, type = "response"))
+
+print(paste("Phi Hat:", round(pearson.phi_hat, 3)))
+# link <- eta
+# response <- mu
